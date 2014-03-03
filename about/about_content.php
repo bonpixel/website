3f13a678eec4a663f504c7c6021c50ba0f1813bb
Bryan Nichols <nicholsbryan@gmail.com>
2014-03-03 15:34:06 -0800

    updating the skill links

--- about/about_content.php
+++ about/about_content.php
@@ -1,35 +1,35 @@
 <div class="shadow">
 					<div class="center" id="aboutheader"><h1>About</h1></div>
-					</div>			
+					</div>
                     <!-- Content -->
 					<div class="shadow">
 						<div id="contentpagewrapper">
-						
+
 							<div id="textdescription">
-							
+
 								<div class="float-right"><img alt=""  src="/images/bryan3.jpg"></div>
 								<h3>A little about me...</h3>
 								<br/>
 								<p>I'm Bryan Nichols, founder and owner of Bonpixel. My dream with Bonpixel is to create an environment where my clients thrive. I want to provide them with competitive web-design packages aimed at making life on the web easier and more productive for them. Whether you are a company or individual, the web provides a unique opportunity to get your message heard.</p>
 								<br/>
-								
+
 								<p>With the web growing faster than any other medium in human history, its important to take advantage of this medium as early as possible. But sometimes best laid plans fail and while many people and businesses have their own websites, they are poorly designed and their message gets lost in the noise. That's where Bonpixel can help. I endeavor to create a web presence that is unique and clear. I will help you communicate effectively to your respective audiences. Take the step toward a more effective future, I want to facilitate you in your endeavor to be heard.</p>
 								<br/>
 								<br/>
 								<p>-Bryan Nichols</p>
-								
+
 							</div>
 							<div class="clear-div"></div>
-							
+
 						</div>
-					
+
 					</div>
-					
+
 						<div class="ishadow">
 						<div class="innerpcontainer">
 						<div class="innershadowtop">
 						<div class="innershadowbottom">
-							
+
 							<div id="cvsection" class="float-left">
 								<h4>Resume</h4>
 								<form>
@@ -39,21 +39,20 @@
 							<div id="skills" class="float-right">
 								<h4>Skills</h4>
 								<ul class="double">
-									<li>HTML</li>
-									<li>XHTML</li>
-									<li>DHTML</li>
-									<li>CSS</li>
+									<li>HTML5</li>
+									<li>CSS3</li>
+									<li>Advanced JavaScript / MV* / jQuery</li>
+									<li>Node / Express</li>
+									<li>Backbone</li>
+									<li>RequireJS / AMD</li>
+									<li>PHP / MySQL</li>
 									<li>Adobe Creative Suite(Photoshop, Illustrator, Etc)</li>
-									<li>JavaScript</li>
-									<li>jQuery</li>
 									<li>Cross Browser Support</li>
-									<li>CMS</li>
-									<li>Wordpress</li>
-									<li>PHP</li>
+									<li>Wordpress / Drupal</li>
 								</ul>
 							</div>
 							<div class="clear-div"></div>
-							
+
 						</div>
 						</div>
 						</div>
