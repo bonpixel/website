04525dca9917003097cdcb10cfdf1a3e82d12ef7
Bryan Nichols <nicholsbryan@gmail.com>
2014-03-03 15:30:33 -0800

    changing the blog link

--- header.php
+++ header.php
@@ -6,35 +6,35 @@
         <title>Bonpixel :: <?=$title ?> </title>
 		<meta name="description" content="Bonpixel Creative: a creative agency specializing in the virtual domain. <?=$description?>" />
 		<meta name="keywords" content="Bonpixel, Web Design, Web Developement, Web Design Buckley, Web Design Seattle, Web Design Bellevue, Pierce County Web Design, Washington Web Design, South Sound Web Design, Buckley Creative Services <?=$keywords?>" />
-	
+
         <!-- The 1140px Grid -->
         <!-- <link rel="stylesheet" href="http://www.bonpixel.com/css/1140.css" type="text/css" media="screen" /> -->
-        
-        
+
+
         <link href="http://www.bonpixel.com/css/reset.css" rel="stylesheet" type="text/css" />
         <link href="http://www.bonpixel.com/css/jquery-ui-1.8.11.custom.css" rel="stylesheet" type="text/css" />
         <link href="http://www.bonpixel.com/css/style.css" rel="stylesheet" type="text/css" />
-        
+
         <script src="http://maps.google.com/maps/api/js?sensor=false" type="text/javascript"></script>
-		
+
 		<script src="https://www.google.com/jsapi?key=ABQIAAAAVrGgPC1mkcRr4_olPUV5shTMbq1HPUTvVsNYRHQg5LWPcakL8hTz4zJQn49IYo8b7yROPYE8gzWZsw" type="text/javascript"></script>
 		<script language="Javascript" type="text/javascript">
 		//<![CDATA[
 
 		google.load("jquery", "1");
 		google.load("jqueryui", "1");
-		
+
 		//]]>
 		</script>
-		
+
 		<script src="http://maps.google.com/maps/api/js?sensor=false" type="text/javascript"></script>
-		
+
         <!-- <script src="http://www.bonpixel.com/scripts/jquery.validate.min.js" type="text/javascript"></script> -->
 		<!-- <script src="http://www.bonpixel.com/scripts/jquery.prettyphoto.js" type="text/javascript"></script> -->
         <!-- <script src="http://www.bonpixel.com/scripts/core.js" type="text/javascript"></script> -->
 		<script src="http://www.bonpixel.com/scripts/core.js" type="text/javascript"></script>
         <script type="text/javascript" src="http://api.recaptcha.net/js/recaptcha_ajax.js"></script>
-	
+
 	<!--[if !IE 7]>
 		<style type="text/css">
 			#wrap {display:table;height:100%}
@@ -46,7 +46,7 @@
 			theme : 'blackglass'
 		 };
 	</script>-->
-		
+
     </head>
 
 
@@ -72,7 +72,7 @@
 								<li><a class="navitem 	portfolionav" 	href="http://www.bonpixel.com/projects" onclick="_gaq.push(['_trackEvent', 'navigation', 'projects']);return true;">Projects</a></li>
 								<li><a class="navitem 	aboutnav" 		href="http://www.bonpixel.com/about" onclick="_gaq.push(['_trackEvent', 'navigation', 'about']);return true;">About</a></li>
 								<li><a class="navitem 	" 				href="http://www.bonpixel.com/contact" onclick="_gaq.push(['_trackEvent', 'navigation', 'contact']);return true;">Contact / Quote</a></li>
-								<li><a class="navitem	" id="blognav" 	href="http://www.bonpixel.com/blog">Blog</a></li>
+								<li><a class="navitem	" id="blognav" 	href="http://blog.bonpixel.com/">Blog</a></li>
 							</ul>
 						</div>
                     </div>
